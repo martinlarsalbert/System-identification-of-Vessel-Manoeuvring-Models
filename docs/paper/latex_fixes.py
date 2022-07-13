50494c96feb597507801d2d5097ad62556a2e4aa
Martin Alexandersson <martin.alexandersson@sspa.se>
2022-07-13 19:15:16 +0200

    equations ref finally fixed! :D

--- docs/paper/latex_fixes.py
+++ docs/paper/latex_fixes.py
@@ -26,6 +26,22 @@ s = s.replace(
     r"\usepackage[,numfigreset=1,mathnumfig]{sphinx}",
 )
 
+"""
+Change to \autoref:
+from: {\hyperref[\detokenize{02.10_propeller_model:equation-eqbetap}]{\sphinxcrossref{(2.19)}}}
+to: \autoref{equation:02.10_propeller_model:eqbetap}
+"""
+
+for result in re.finditer(
+    r"\{\\hyperref\[\\detokenize\{([^}]+)}]{\\sphinxcrossref{[^}]+}}}", s
+):
+
+    if ":equation" in result.group(1):
+        notebook_name = result.group(1).split(":")[0]
+        eq_label = result.group(1).split("equation-")[-1]
+        s2 = r"\autoref{equation:" + notebook_name + ":" + eq_label + "}"
+        s = s.replace(result.group(0), s2)
+
 
 ## Add fontspec
 s = s.replace(
